c67dcbf179e07c131b6c0e2f15089a38cb3ebf5e
harshithamullagiri <harshithamullagiri7@gmail.com>
2022-07-01 18:49:25 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+export function App() {
+	return (
+		<ChatEngine
+		height='100vh'
+		userName='Harshitha'
+		userSecret='123123'
+		projectID='339a33e5-04a8-4390-8ae1-1afa5f1d7f53'
+		/>
+	);
+}
+export default  App;
